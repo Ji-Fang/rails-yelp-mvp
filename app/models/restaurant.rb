7d994eec59506b986a5d12b0dabed66165b8e5dd
JiFang Lo <jennylo2477@gmail.com>
2024-08-08 15:56:34 +0200

    rails new

--- app/models/restaurant.rb
+++
@@ -1,4 +0,0 @@
-class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destroy
-  validates :name, presence: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_07_123725) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_07_094202) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -22,13 +22,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_07_123725) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.text "content"
-    t.integer "restaurant_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
-  end
-
-  add_foreign_key "reviews", "restaurants"
 end
